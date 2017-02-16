388f38c6a82ca7d85b47860a97e65307d2f737df
Jeffrey Woodard <icikalu@gmail.com>
2017-02-16 10:18:43 -0600

    add shop by category by navbar

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,8 +1,20 @@
 class StorefrontController < ApplicationController
   def index
-    @products = Product.all
+    if params[:cat_id]
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else
+      @products = Product.all
+    end
   end
 
   def about
   end
 end
+
+
+
+
+
+
+
