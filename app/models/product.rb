2d36568759afee8ef2536d282726f5ad1af77679
Jeffrey Woodard <icikalu@gmail.com>
2017-02-20 09:09:25 -0600

    set up line item and cart models, tables and relationships.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :brand, optional: true
   belongs_to :category, optional: true
+  has_mamy :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" },
                             default_url: "missing_:style.jpeg"
@@ -12,14 +14,19 @@ class Product < ApplicationRecord
 
   def self.search_by_name_for_description(string)
     where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%")
-
   end
 
-  # Fields
-  # name - presence
-  # description - presence
-  # category - presence
-  # brand - presence
-  # price - >= 0.01
-  # quantity - >= 0
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items Present')
+      return false
+    end
+  end
+
 end
+
+
