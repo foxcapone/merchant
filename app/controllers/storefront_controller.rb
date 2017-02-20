89cc4d2e2d92a9d7a8ea80dc4e490e049b3bcabf
Jeffrey Woodard <icikalu@gmail.com>
2017-02-20 08:50:16 -0600

    update storefron models and controllers

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,12 @@
 class StorefrontController < ApplicationController
   def index
-    if params[:cat_id]
+    if params[:search]
+      @products = Product.search_by_name_for_description(params[:search])
+      if @products.empty?
+        flash.now[:notice] = "No result found for \"#{params[:search]}\", showing all products."
+        @products = Product.all
+      end
+    elsif params[:cat_id]
       @category = Category.find(params[:cat_id])
       @products = Product.where(category_id: params[:cat_id])
     else
@@ -11,6 +17,9 @@ class StorefrontController < ApplicationController
   def about
   end
 end
+
+
+
 
 
 
