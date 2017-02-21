1055e3b3b0ef901f019bd368619debf6bd3508c2
Jeffrey Woodard <icikalu@gmail.com>
2017-02-21 08:53:34 -0600

    add method to update cart quantity

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,21 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
+  # Adds increment quantity if product is already in cart
+  # Add new line item if product isnt in cart
+  def add_product(product_id)
+    product = Product.find(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product: product, price: product.price)
+    end
+
+    current_item
+  end
+
   def subtotal
     # SQL version
     line_items.select("SUM(quantity * price) AS sum")[0].sum
